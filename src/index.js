dc75d6ef012832355f807aa0ac47a0a49e32a185
Anna Mugu <86118286+doublexkid@users.noreply.github.com>
2021-08-28 21:50:58 +0300

    edit: add solution 17/20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let current;
+  const Brackets_Pair = {
+      "(": ")", 
+      "[": "]", 
+      "{": "}",
+
+  };
+  
+  for (let i = 0; i < str.length; i++) {
+    current = str[i]; 
+    
+    if (current === '(' || current === '[' || current === "{" ) {
+      stack.push(current);
+      
+    } else if (current === ')' || current === ']' || current === "}" ) {
+      const lastBracket = stack.pop();
+      
+      if (Brackets_Pair[lastBracket] !== current) { 
+      
+        return false; 
+        
+      }
+    }
+   
+    
+  }
+  
+  return stack.length === 0;
 }
